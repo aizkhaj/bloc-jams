b5b5747e9667280cf3f931a5a3b1115a9a8d4fc8
Aizaz Khaja <aizazkj@gmail.com>
2016-09-25 02:06:38 -0600

    assign11 complete. the page has a cool albumview loop/toggle feature

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,21 @@ var albumMarconi = {
     ]
 };
 
+var albumJazzy = {
+    title: 'The Jazz',
+    artist: 'Jazz Man',
+    label: 'someJazz',
+    year: '2018',
+    albumArtUrl: 'assets/images/album_covers/12.png',
+    songs: [
+        {title: 'Hey, its jazz', duration: '1:01'},
+        {title: 'Ringalingding', duration: '5:01'},
+        {title: 'Smooth', duration: '3:21'},
+        {title: 'Totally Smooth', duration: '3:14'},
+        {title: 'Jazz to the sna', duration: '2:15'}
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
      '<tr class="album-view-song-item">'
@@ -40,13 +55,14 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 var setCurrentAlbum = function(album) {
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-    
+        
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -61,4 +77,13 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+    var albums = [albumPicasso, albumMarconi, albumJazzy];
+    var index = 1;
+    albumImage.addEventListener('click', function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if (index == albums.length) {index = 0};
+    });
+
 };
