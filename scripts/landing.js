6e1296f85ebd4f4fe47125729098e572ea10b38a
Aizaz Khaja <aizazkj@gmail.com>
2016-10-05 01:17:09 -0600

    chk16 complete; landing.js refactored with jQuery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,3 +1,4 @@
+/*
 var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
@@ -11,16 +12,28 @@ var animatePoints = function(points) {
 	};
 	revealPoint();
 };
+*/
 
-window.onload = function() {
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    $.each($('.point'), revealPoint);
+};
+
+$(window).load(function() {
+    if ($(window).height() > 950) {
+        animatePoints();
     }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
-        }
+    
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+    $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
+        } 
     });
-}
+});
